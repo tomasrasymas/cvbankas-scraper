7f083bfccabe1b7e2a27ab2a65b9c949e5d23984
tomasrasymas <tomas@dutrys.com>
2018-02-01 10:28:24 +0200

    version 0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="cvbankas_scraper",
-      version="0.0.2",
+      version="0.0.3",
       description="cvbankas.lt web site scraper",
       url="https://github.com/tomasrasymas/cvbankas_scraper",
       author="Tomas Rasymas",
